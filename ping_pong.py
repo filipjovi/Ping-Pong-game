9b7a8936c3f9b23e5cc27e13c6d84f0a2940191d
Filip <136001126+filipjovi@users.noreply.github.com>
2023-06-09 20:22:51 +0200

    template2
    v2.0

--- ping_pong.py
+++ ping_pong.py
@@ -1,5 +1,33 @@
 from pygame import *
 
+class GameSprite(sprite.Sprite):
+    def __init__(self,player_image,player_x,player_y,width,height,player_speed):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image),(width,height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP]and self.rect.y > 5 :
+            self.rect.y - self.speed
+        if keys[K_DOWN]and self.rect.y < window_height -80:
+            self.rect.y += self.speed
+    
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w]and self.rect.y > 5 :
+            self.rect.y - self.speed
+        if keys[K_s]and self.rect.y < window_height -80:
+            self.rect.y += self.speed
+
+        
+
 back = (200,255,255)
 window_width = 600
 window_height = 500
@@ -17,4 +45,5 @@ while game:
 
     display.update()
     clock.tick(FPS)
+
 
